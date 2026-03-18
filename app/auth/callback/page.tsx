d838ad9da4cce1a33febff74c9ff097b17924198
Devmouraa <williamjose18n@gmail.com>
2026-03-18 18:42:34 -0300

    Callback

---
+++ app/auth/callback/page.tsx
@@ -0,0 +1,9 @@
+export default function AuthCallbackPage() {
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '60vh' }}>
+      <h1>Autenticando...</h1>
+      <p>Por favor, aguarde enquanto processamos seu login.</p>
+    </div>
+  );
+}
